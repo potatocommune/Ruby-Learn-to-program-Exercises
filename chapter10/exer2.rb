1e283f7fba685a5a027ca00abc68f63915e8a0b0
Boyang Li <nian1538@gmail.com>
2017-05-22 10:42:25 +0800

    Run directly from OS

--- chapter10/exer2.rb
+++ chapter10/exer2.rb
@@ -1,21 +1,24 @@
+# !/usr/bin/env ruby
+
 def log block_description, &block
   $layer = $layer + 1
   puts "  " * $layer + "Beginning \"#{block_description}\"..."
   puts "  " * $layer + "...\"#{block_description}\" finished, returning:  #{block.call}"
-
   $layer = $layer - 1
 end
 
-$layer = 0
-log "outer block" do
-  log "some little block" do
-    5
-    log "tiny tiny block" do
-      "lots of love"
+if $PROGRAM_NAME == __FILE__
+  $layer = 0
+  log "outer block" do
+    log "some little block" do
+      5
+      log "tiny tiny block" do
+        "lots of love"
+      end
     end
-  end
 
-  log "yet another block" do
-    "I like Thai food"
+    log "yet another block" do
+      "I like Thai food"
+    end
   end
 end
